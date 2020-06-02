3d215e7f93cde4f75cd7c87560aae607b83a33d4
Alex Platteeuw <aplatteeuw@outlook.fr>
2020-06-02 21:24:23 +0200

    books#create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,29 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def new
     @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.lender = current_user
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :published_date, :rating, :publisher, :category)
   end
 
 end
