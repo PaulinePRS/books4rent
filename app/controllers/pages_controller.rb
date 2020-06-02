18bed5bb2611d23c45ee8311ef2289d720413375
clement lemaire <c.lemaire027@gmail.com>
2020-06-02 18:18:30 +0200

    working on the banner and the seach form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  # def index 
+  #   if params[:query] 
+  #     @query = params[:query] 
+  #     @books = Book.where("name ILIKE '%#{params[:query]}%'") 
+  #   else 
+  #     @books = Book.order(:name) 
+  # end
 end
