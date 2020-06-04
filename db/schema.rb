58aa8506a92dee005501f291bd92acd20345e456
Maxime Berthelot <maxime@buffer.com>
2020-06-04 21:58:04 +0200

    fixed popo error

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_06_04_190238) do
-
-ActiveRecord::Schema.define(version: 2020_06_04_171805) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_171805) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.bigint "book_id", null: false
@@ -49,7 +45,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_171805) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["book_id"], name: "index_bookings_on_book_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
-
   end
 
   create_table "books", force: :cascade do |t|
@@ -89,12 +84,9 @@ ActiveRecord::Schema.define(version: 2020_06_04_171805) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "bookings", "books"
   add_foreign_key "bookings", "users"
-
   add_foreign_key "books", "users", column: "lender_id"
   add_foreign_key "reviews", "books"
 end
