f2b3348011f60f3e43255d8ee9692b95b49bbcbe
bart boillot <barthboillot@hotmail.com>
2020-06-04 22:03:09 +0200

    authentification for edit and destroy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_171805) do
+ActiveRecord::Schema.define(version: 2020_06_04_212642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,9 @@ ActiveRecord::Schema.define(version: 2020_06_04_171805) do
     t.bigint "book_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,4 +69,5 @@ ActiveRecord::Schema.define(version: 2020_06_04_171805) do
   add_foreign_key "bookings", "users"
   add_foreign_key "books", "users", column: "lender_id"
   add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
