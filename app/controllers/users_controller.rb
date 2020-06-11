bd5bad10b6623157a9f1f5da8aef080d3b133371
Alex Platteeuw <aplatteeuw@outlook.fr>
2020-06-11 09:33:42 +0200

    fix pundit authorization for my books section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    if @user == current_user
-      @books = @user.books
-      authorize @user
-    else
-      redirect_to books_path
-    end
+    @user == current_user
+    @books = @user.books
+    authorize @user
   end
 end
